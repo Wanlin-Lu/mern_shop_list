4dccce4d73e37ef133fc6c70cfa708adc9930d69
Wanlin-Lu <luwanlin@126.com>
2020-06-03 11:30:21 +0800

    mongoose connect to mongoDB

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 const app = express()
 
 app.use(bodyParser.json())
+
+const db = require('./config/keys').mongoURI
+mongoose.connect(
+  db,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
 
 const port = process.env.PORT || 5000
 
