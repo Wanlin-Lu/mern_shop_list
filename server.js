63a07908cc7be43c2a70b0f79c834b9515e8bf7f
Wanlin-Lu <luwanlin@126.com>
2020-06-04 14:27:56 +0800

    prepare for heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const path = require('path')
 
 const ItemRouter = require('./routes/api/items')
 
@@ -16,6 +17,14 @@ app.use((req, res, next) => {
 })
 
 app.use('/api/items', ItemRouter)
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 const db = require('./config/keys').mongoURI
 mongoose.connect(
